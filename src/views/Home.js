1de5659a3ef985704bf5e567d27d8319b60de15e
Adrian Dinis <a44721@alunos.ipb.pt>
2020-09-26 12:05:04 +0100

    Get users from fake API

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class Home extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentWillMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        this.setState({users: response.data})
+      })
+  }
   render() {
     return (
       <h1>Home</h1>
